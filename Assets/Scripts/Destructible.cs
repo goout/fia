03b2ac204cf0e43bb211093e3a1d03163a16d449
goout <goout@bk.ru>
2020-02-16 22:04:36 +0300

    more shakes

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -7,7 +7,7 @@ public class Destructible : MonoBehaviour
 
 bool isShaking = false;
 Vector2 startPos;
-float shakeAmount = .035f;
+float shakeAmount = .06f;
     // Start is called before the first frame update
     void Start()
     {
