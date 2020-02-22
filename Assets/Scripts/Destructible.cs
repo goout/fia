c5ca5518e46aecc14085ba77dfa71e6e21d59028
goout <goout@bk.ru>
2020-02-22 13:45:08 +0300

    Destructible object

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
+[SerializeField] int health = 3;
 
+[SerializeField] UnityEngine.Object destructableRef;
 bool isShaking = false;
 Vector2 startPos;
 float shakeAmount = .06f;
@@ -24,14 +27,26 @@ float shakeAmount = .06f;
 
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.name == "AttackCollider") {
-           Debug.Log("hit");
+        if (collision.CompareTag("Weapon")) {
            isShaking = true;
            Invoke("StopShaking", .5f);
+           health--;
+                   if (health <= 0) {
+               DestructGameObject();
+           }
         }
     }
+
+    private void DestructGameObject()
+    {
+        GameObject destructable = (GameObject)Instantiate(destructableRef);
+        destructable.transform.position = transform.position;
+        Destroy(gameObject);
+    }
+
     void StopShaking() {
         isShaking = false;
         transform.position = startPos;
+
     }
 }
---
+++ Assets/Scripts/DestructibleRigidbody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleRigidbody : MonoBehaviour
+{
+[SerializeField] Vector2 forceDirection;
+[SerializeField] float torque;
+Rigidbody2D rigid2D;
+
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        rigid2D.AddForce(forceDirection);
+        rigid2D.AddTorque(torque);
+    }
+
+ 
+    void Update()
+    {
+      
+    }
+}
