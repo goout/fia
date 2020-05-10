21250ee9222a6cb770f5749ea4a46002a9629032
goout <goout@bk.ru>
2020-05-10 14:47:22 +0300

    enemy sketch

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int speed;
+    [SerializeField] protected int gems;
+    [SerializeField] protected Transform pointA, pointB;
+
+    protected Vector3 currentTarget;
+    protected Animator animator;
+    protected SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            return;
+        Movement();
+    }
+
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public virtual void Movement()
+    {
+        if (currentTarget == pointA.position)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.transform.position)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+}
